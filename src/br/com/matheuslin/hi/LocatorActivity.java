1e1fd9b9d284f723d8547d303cced86484bb2c8a
matheuslin <matheus.lin@gmail.com>
2012-10-09 01:37:46 -0300

    Latitude and Longitude are now being displayed on "Localize" activity! The button does nothing yet, though. =P

--- src/br/com/matheuslin/hi/LocatorActivity.java
+++ src/br/com/matheuslin/hi/LocatorActivity.java
@@ -2,27 +2,68 @@ package br.com.matheuslin.hi;
 
 import android.app.Activity;
 import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LocatorActivity extends Activity {
 
-	LocationManager locationManager;
+	TextView latitudeText;
+	TextView longitudeText;
 	
+	LocationManager locationManager;
+	LocationListener locationListener;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
         
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);        
+        latitudeText = (TextView) findViewById(R.id.latitudeValue);
+    	longitudeText = (TextView) findViewById(R.id.longitudeValue);
+    	
+    	locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+    	
+    	locationListener = new LocationListener(){
+
+    		@Override
+    		public void onLocationChanged(Location location) {
+    			makeUseOfNewLocation(location);
+    		}
+
+    		@Override
+    		public void onProviderDisabled(String provider) {
+    			
+    		}
+
+    		@Override
+    		public void onProviderEnabled(String provider) {
+    			
+    		}
+
+    		@Override
+    		public void onStatusChanged(String provider, int status, Bundle extras) {
+    			
+    		}
+    		
+    	};
+    	
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 0, locationListener);
     }
 
-    @Override
+    protected void makeUseOfNewLocation(Location location) {
+		latitudeText.setText(String.format("%.8f", location.getLatitude()));
+		longitudeText.setText(String.format("%.8f", location.getLongitude()));
+	}
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_location, menu);
         return true;
@@ -40,7 +81,6 @@ public class LocatorActivity extends Activity {
     }
     
     public void getLocation(View view){
-    	
     }
 
 }
